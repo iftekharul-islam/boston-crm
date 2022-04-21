0d597c94eaffb9dcbc8bd47b983eda10cb0fc168
mashuk-aug <mushfiqur@augnitive.com>
2022-04-21 16:54:22 +0600

    changed app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,39 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
-import Vue from 'vue';
-window.Vue = Vue;
-import VueRouter from 'vue-router';
-import default_routes from './routes/routes';
-import { i18n } from "./i18nPlugin/index";
-import { storage } from './store/index';
-import "./vee-validate/index";
 
-Vue.use(VueRouter);
-Vue.config.productionTip = false;
+window.Vue = require('vue').default
+import Pagination from 'vue-pagination-2'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-const Home = () => import(/* webpackChunkName : "Home" */ './components/Init');
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
 
-const router = new VueRouter({
-    default_routes,
-    mode: 'history',
-    scrollBehavior() {
-        return { x: 0, y: 0 }
-    }
-});
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+Vue.use(VueSweetalert2);
+Vue.component('pagination', Pagination);
+Vue.component('clients-list', require('./components/clients/list').default);
+Vue.component('clients-create', require('./components/clients/create').default);
 
-router.beforeEach((to, from, next) => {
-    let title = to.meta.title;
-    if (title) {
-        document.title = title + ' | ' + storage.state.app.appName
-    } else {
-        document.title = storage.state.app.appName
-    }
-    next();
-});
+
+Vue.config.productionTip = false
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
 
 const app = new Vue({
     el: '#app',
-    i18n,
-    router,
-    storage,
-    components: {
-        'init-app': Home
-    }
 });
