18b2497de993675a5f31a8b8c9fdcdd2f94568b4
MD Minhaj <minhaj372@gmail.com>
2022-04-27 14:22:40 +0600

    User preview design

--- resources/views/user/_user-profile-modal.blade.php
+++ resources/views/user/_user-profile-modal.blade.php
@@ -1,19 +1,58 @@
-<div id="userProfile" class="modal fade" tabindex="-1">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="userName"></h5>
-                <button type="button" class="btn close" data-dismiss="modal" onclick="profileModalClose();">
-                    &times;
+<div id="userProfile" class="modal fade user-prev" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered user-prev__dialog">
+        <div class="modal-content user-prev__content">
+                <button type="button" class="btn close close-btn" data-dismiss="modal" onclick="profileModalClose();">
+                    <span class="icon-close-circle fs-24"><span class="path1"></span><span class="path2"></span></span>
                 </button>
-            </div>
-            <div class="modal-body">
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary"
-                        data-dismiss="modal">{{ __('messages.close') }}</button>
-            </div>
+                <div class="user-prev-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h4 class="fs-20 fw-bold text-light-black mgb-20">User details</h4>
+                            <div class="user-prev-img">
+                                <img src="{{ asset('img/user.png') }}" alt="User image" class="img-fluid">
+                            </div>
+                            <div class="group">
+                                <p class="mgb-12 text-light-black">Company name</p>
+                                <p class="mb-0 fw-bold text-light-black">Boston CRM</p>
+                            </div>
+                            <div class="group">
+                                <p class="mgb-12 text-light-black">Name</p>
+                                <p class="mb-0 fw-bold text-light-black">Boston CRM</p>
+                            </div>
+                            <div class="group">
+                                <p class="mgb-12 text-light-black">Address</p>
+                                <p class="mb-0 fw-bold text-light-black">1453 Dorchester Avenue, Boston, MA 02122
+                                    PO Box 240889, Boston, MA 02124</p>
+                            </div>
+                            <div class="group">
+                                <p class="mgb-12 text-light-black">Joining date</p>
+                                <p class="mb-0 fw-bold text-light-black">Boston CRM</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="group">
+                                <p class="mgb-12 text-light-black">City</p>
+                                <p class="mb-0 fw-bold text-light-black">Boston CRM</p>
+                            </div>
+                            <div class="group">
+                                <p class="mgb-12 text-light-black">State</p>
+                                <p class="mb-0 fw-bold text-light-black">Boston CRM</p>
+                            </div>
+                            <div class="group">
+                                <p class="mgb-12 text-light-black">Phone</p>
+                                <p class="mb-0 fw-bold text-light-black">Boston CRM</p>
+                            </div>
+                            <div class="group">
+                                <p class="mgb-12 text-light-black">Email</p>
+                                <p class="mb-0 fw-bold text-light-black">Boston CRM</p>
+                            </div>
+                            <div class="group">
+                                <p class="mgb-12 text-light-black">Total task</p>
+                                <p class="mb-0 fw-bold text-light-black">Boston CRM</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
 </div>
