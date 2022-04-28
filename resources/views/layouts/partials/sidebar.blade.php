474fafb04ec776bab8265f8fb5b723ae39282efb
mashuk-aug <mushfiqur@augnitive.com>
2022-04-28 10:21:07 +0600

    Menu hide and login back button hide on login page

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -52,13 +52,13 @@
              </div>
              <span class="icon-arrow-down ms-auto"></span>
         </a>
-         <a href="{{ route('orders.index') }}" class="list-item d-flex align-items-center  text-white {{ (request()->is('orders*')) ? 'active' : '' }}">
-            <div class="d-inline-flex align-items-center">
-                <span class="icon-order me-3 fs-3"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span>
-                <span class="items-text"> {{ __('messages.dashboard_view.orders') }} </span>
-             </div>
-             <span class="icon-arrow-down ms-auto"></span>
-        </a>
+{{--         <a href="{{ route('orders.index') }}" class="list-item d-flex align-items-center  text-white {{ (request()->is('orders*')) ? 'active' : '' }}">--}}
+{{--            <div class="d-inline-flex align-items-center">--}}
+{{--                <span class="icon-order me-3 fs-3"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span>--}}
+{{--                <span class="items-text"> {{ __('messages.dashboard_view.orders') }} </span>--}}
+{{--             </div>--}}
+{{--             <span class="icon-arrow-down ms-auto"></span>--}}
+{{--        </a>--}}
 {{--         <a href="{{ url('/marketing') }}" class="list-item d-flex align-items-center  text-white">--}}
 {{--            <div class="d-inline-flex align-items-center">--}}
 {{--                <span class="icon-ranking me-3 fs-3"></span>--}}
--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -193,6 +193,7 @@
             let model_name = self.data('model-name');
             let model_list = ["create." + model_name, "update." + model_name, "delete." + model_name];
             let view_select;
+            console.log(name)
             if (name.includes('.' + model_name)) {
                 view_select = checkView(name, self, all_siblings, model_list);
                 if (view_select && $(self).is(':checked')) {
