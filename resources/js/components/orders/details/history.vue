01a3b1eba9fb4d413a1c40554bee6b6b4727ec45
mashuk-aug <mushfiqur@augnitive.com>
2022-05-12 14:47:27 +0600

    order details templating

---
+++ resources/js/components/orders/details/history.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="order-details__right col-md-4">
+    History
+    <div class="order-details-box bg-white h-100">
+      <div class="box-header">
+        <p class="fw-bold text-light-black fs-20 mb-0">History</p>
+
+      </div>
+      <div class="box-body">
+        <p class="mb-3 text-light-black">Order Created by: <span class="text-600 text-light-black">Toushi</span></p>
+        <p class="mb-3 text-light-black">Scheduled by: <span class="text-600 text-light-black">Toushi</span></p>
+        <p class="mb-3 text-light-black">Inspected by: <span class="text-600 text-light-black">Toushi</span></p>
+        <p class="mb-3 text-light-black">prepared by: <span class="text-600 text-light-black">Toushi</span></p>
+        <p class="mb-3 text-light-black">Initially Reviewed by: <span class="text-600 text-light-black">Toushi</span></p>
+        <p class="mb-3 text-light-black">Report Analysed and Reviewed by: <span class="text-600 text-light-black">Toushi</span></p>
+        <p class="mb-3 text-light-black">Quality Assured by: <span class="text-600 text-light-black">Toushi</span></p>
+        <p class="mb-3 text-light-black">Delivered by: <span class="text-600 text-light-black">Toushi</span></p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ resources/js/components/orders/details/notes.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="order-details-box bg-white">
+    <div class="box-header">
+      <p class="fw-bold text-light-black fs-20 mb-0">Notes</p>
+    </div>
+    <div class="box-body">
+      <div class="note-chat">
+        item chat
+        <div class="chat-item">
+          <div class="chat-name d-flex align-items-center">
+            <img src="/img/dummy-profile.png" alt="boston chat image" class="img-fluid">
+            <div class="ms-3">
+              <p class="text-600 mb-1">Mahfuz</p>
+              <span class="text-gray">Section 1</span>
+            </div>
+          </div>
+          chat
+          <div class="d-inline-block message">
+            <p class="mb-0 ">Kmon achen vaiya?</p>
+          </div>
+        </div>
+        item chat
+        <div class="chat-item">
+          <div class="chat-name d-flex align-items-center">
+            <img src="/img/dummy-profile.png" alt="boston chat image" class="img-fluid">
+            <div class="ms-3">
+              <p class="text-600 mb-1">Mofiz</p>
+              <span class="text-gray">Section 1</span>
+            </div>
+          </div>
+          chat
+          <div class="d-inline-block message">
+            <p class="mb-0 ">Valo nai re vai . ki korbo kichui bujtachina , valo lagena dunia dari. jai hok tor ki obostha</p>
+          </div>
+        </div>
+        item chat
+        <div class="chat-item">
+          <div class="chat-name d-flex align-items-center">
+            <img src="/img/dummy-profile.png" alt="boston chat image" class="img-fluid">
+            <div class="ms-3">
+              <p class="text-600 mb-1">Mahfuz</p>
+              <span class="text-gray">Section 1</span>
+            </div>
+          </div>
+          chat
+          <div class="d-inline-block message">
+            <p class="mb-0 ">Kmon achen vaiya?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
