002a102ea2f19ebd1264780310037a8c53339a4b
mashuk-aug <mushfiqur@augnitive.com>
2022-05-16 15:00:33 +0600

    client issues fixed

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <clients-list :permissions="{{ json_encode($user_permissions) }}" :isOwner="{{ $is_owner }}" :role="'{{ $user_role }}'" :show-route="'{{ route('clients.show', [""]) }}'" :create-route="'{{ route('clients.create') }}'" :delete-route="'{{ route('clients.destroy', [""]) }}'"></clients-list>
+{{--    @dd($is_owner)--}}
+    @php
+        $owner = $is_owner ? 'true' : 'false'
+    @endphp
+{{--@dd($owner)--}}
+    <clients-list :permissions="{{ json_encode($user_permissions) }}" :is-owner="{{ $owner }}" :role="'{{ $user_role }}'" :show-route="'{{ route('clients.show', [""]) }}'" :create-route="'{{ route('clients.create') }}'" :delete-route="'{{ route('clients.destroy', [""]) }}'"></clients-list>
 @endsection
