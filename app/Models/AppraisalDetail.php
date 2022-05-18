963ef4c54af7321c5f4ac8ed88ce961f5fc3057d
mashuk-aug <mushfiqur@augnitive.com>
2022-05-18 14:56:57 +0600

    model fillable property added

--- app/Models/AppraisalDetail.php
+++ app/Models/AppraisalDetail.php
@@ -10,7 +10,7 @@ class AppraisalDetail extends Model
     use HasFactory;
 
     protected $fillable = [
-        'order_id','appraiser_id','client_order_no','loan_no','loan_type','received_date','due_date',
-        'technology_type','fha_case_no'
+        'order_id','appraiser_type_id','system_order_no','client_order_no','loan_no','loan_type','received_date','due_date',
+        'technology_fee','fha_case_no'
     ];
 }
