bf2a0eee6e0005484d00e1193ead2616da5b8c92
mashuk-aug <mushfiqur@augnitive.com>
2022-06-09 12:30:42 +0600

    conflict solve in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,42 +1,34 @@
 require('./bootstrap');
 window.Vue = require('vue').default
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-<<<<<<< HEAD
-=======
-import 'vue-select/dist/vue-select.css';
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
->>>>>>> production
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import { ValidationProvider } from "vee-validate/dist/vee-validate.full.esm";
-import { ValidationObserver } from 'vee-validate';
-Vue.component('ValidationProvider', ValidationProvider);
-Vue.component('ValidationObserver', ValidationObserver);
+import 'vue-select/dist/vue-select.css'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
-import storage from "./store/index";
-import * as boston from "./helper/boston";
-import "./helper/config";
+import { ValidationProvider } from "vee-validate/dist/vee-validate.full.esm"
+import { ValidationObserver } from 'vee-validate'
+
+import storage from "./store/index"
+import * as boston from "./helper/boston"
+import "./helper/config"
+import "./src/vue_component"
+import "./helper/BostonMixin"
+
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
+Vue.component('v-select', vSelect)
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-<<<<<<< HEAD
+Vue.use(VueSweetalert2)
 
-// import vSelect from 'vue-select';
-// Vue.component('v-select', vSelect)
-=======
-Vue.component('v-select', vSelect)
-Vue.use(VueSweetalert2);
->>>>>>> production
-
-import "./src/vue_component";
-
-axios.defaults.baseURL = window.origin;
 Vue.config.productionTip = false
-Vue.prototype.$boston = boston;
+Vue.prototype.$boston = boston
 
-import "./helper/BostonMixin";
+axios.defaults.baseURL = window.origin
 
 const app = new Vue({
     el: '#app',
