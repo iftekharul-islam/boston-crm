831887b9651e3d49d60335fb3820e3324753faec
mashuk-dev <mushfiqur.rahman06@gmail.com>
2022-06-27 19:42:57 +0600

    client fixes and files fixes

---
+++ app/Rules/UniqueName.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Client;
+use Illuminate\Support\Arr;
+use Illuminate\Contracts\Validation\Rule;
+
+class UniqueEmail implements Rule
+{
+    protected $company_id;
+    protected $client_emails;
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct($client_id)
+    {
+        $this->client_id = $client_id;
+        ($this->client_id > 0) ? $this->client_emails = json_decode(Client::whereNotNull('email')->where('id','!=', $this->client_id)->pluck('email'),true)
+            : $this->client_emails = json_decode(Client::whereNotNull('email')->pluck('email'),true);
+
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        $passed = true;
+        if($this->array_has_duplicates($value)){
+            return false;
+        }
+        foreach ($this->client_emails as $email){
+            $decoded_email = !is_array($email) ? [$email] : json_decode($email, true);
+            foreach ($decoded_email as $emailStr){
+                $d_email = json_decode($emailStr);
+                if(array_intersect($d_email, $value)) {
+                    $passed = false;
+                }
+            }
+        }
+        return $passed;
+    }
+
+    /**
+     * @param $array
+     * @return bool
+     */
+    public function array_has_duplicates($array): bool
+    {
+        return count($array) !== count(array_unique($array));
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'This email has already taken/duplicate email.';
+    }
+}
--- resources/js/components/orders/details/files.vue
+++ resources/js/components/orders/details/files.vue
@@ -1,103 +1,119 @@
 <template>
-  <div class="row ">
-    <div class="col-12">
-      <!-- Files -->
-      <div class="order-details-box bg-white">
-        <div class="box-header">
-          <p class="fw-bold text-light-black fs-20 mb-0">Files</p>
-          <div class="group">
-            <div v-b-modal.upload-files class="position-relative file-upload document-upload">
-              <label for="">Upload <img src="/img/upload.png" alt="boston profile"></label>
-            </div>
-          </div>
-        </div>
-        <div class="box-body">
-          <!-- document -->
-          <div class="document">
-            <div class="row">
-              <div class="col-sm-6 col-md-4 col-lg-3" v-for="(files,type) in orderFiles" :key="type">
-                <p class="fw-bold text-light-black">{{ type }}</p>
-                <div class="d-flex align-items-center mb-3" v-for="file in files">
-                  <img src="/img/pdf.svg" alt="boston profile" class="img-fluid">
-                  <span class="text-light-black d-inline-block mgl-12 file-name"><a :href="file.original_url" download>{{ file.name }}</a></span>
+    <div class="row ">
+        <div class="col-12">
+            <!-- Files -->
+            <div class="order-details-box bg-white">
+                <div class="box-header">
+                    <p class="fw-bold text-light-black fs-20 mb-0">Files</p>
+                    <div class="group">
+                        <div v-b-modal.upload-files class="position-relative file-upload document-upload">
+                            <label for="">Upload <img src="/img/upload.png" alt="boston profile"></label>
+                        </div>
+                    </div>
                 </div>
-              </div>
+                <div class="box-body">
+                    <!-- document -->
+                    <div class="document">
+                        <div class="row">
+                            <div class="col-sm-6 col-md-4 col-lg-3" v-for="(files,type) in allFiles" :key="type">
+                                <p class="fw-bold text-light-black">{{ type }}</p>
+                                <div class="d-flex align-items-center mb-3" v-for="file in files">
+                                    <img src="/img/pdf.svg" alt="boston profile" class="img-fluid">
+                                    <span class="text-light-black d-inline-block mgl-12 file-name"><a
+                                            :href="file.original_url" download>{{ file.name }}</a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
+        <b-modal id="upload-files" size="lg" title="Uploads Order Files">
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <ValidationObserver ref="fileForm">
+                            <ValidationProvider class="group d-block" name="File type" rules="required"
+                                v-slot="{ errors }">
+                                <div :class="{ 'invalid-form' : errors[0] }">
+                                    <label for="" class="d-block mb-2 dashboard-label">Select file type <span
+                                            class="require"></span></label>
+                                    <b-form-select v-model="fileData.file_type" :options="orderFileTypes"
+                                        class="dashboard-input w-100">
+                                        <template #first>
+                                            <b-form-select-option value="" disabled>-- Please select an option --
+                                            </b-form-select-option>
+                                        </template>
+                                    </b-form-select>
+                                </div>
+                                <span v-if="errors[0]" class="error-message">{{ errors[0] }}</span>
+                            </ValidationProvider>
+                        </ValidationObserver>
+                        <div class="group">
+                            <label for="" class="d-block mb-2 dashboard-label">Select file <span
+                                    class="require"></span></label>
+                            <input type="file" multiple v-on:change="addFiles">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div slot="modal-footer">
+                <b-button variant="secondary" @click="$bvModal.hide('upload-files')">Close</b-button>
+                <b-button variant="primary" @click="saveOrderFiles">Save</b-button>
+            </div>
+        </b-modal>
     </div>
-    <b-modal id="upload-files" size="lg" title="Uploads Order Files">
-      <div class="modal-body">
-        <b-alert v-if="message" show variant="success"><a href="#" class="alert-link">{{ message }}</a></b-alert>
-        <div class="row">
-          <div class="col-md-6">
-            <div class="group">
-              <label for="" class="d-block mb-2 dashboard-label">Select file type <span class="require"></span></label>
-              <b-form-select
-                  v-model="fileData.file_type"
-                  :options="orderFileTypes"
-                  class="dashboard-input w-100">
-                <template #first>
-                  <b-form-select-option value="" disabled>-- Please select an option --</b-form-select-option>
-                </template>
-              </b-form-select>
-            </div>
-            <div class="group">
-              <label for="" class="d-block mb-2 dashboard-label">Select file <span class="require"></span></label>
-              <input type="file" multiple v-on:change="addFiles">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div slot="modal-footer">
-        <b-button variant="secondary" @click="$bvModal.hide('upload-files')">Close</b-button>
-        <b-button variant="primary" @click="saveOrderFiles">Save</b-button>
-      </div>
-    </b-modal>
-  </div>
 </template>
 <script>
-  export default {
-    props:{
-      orderId: String,
-      orderFileTypes: [],
-      orderFiles: []
-    },
-    data(){
-      return{
-        fileData:{
-          file_type: '',
-          files: [],
+    export default {
+        props: {
+            orderId: String,
+            orderFileTypes: [],
+            orderFiles: []
         },
-        message: ''
-      }
-    },
-    methods:{
-      addFiles(event){
-        this.fileData.files = event.target.files
-      },
-      saveOrderFiles(){
-        let that = this
-        let formData = new FormData();
-        for( let i = 0; i < this.fileData.files.length; i++ ){
-          let file = this.fileData.files[i];
-          formData.append('files[' + i + ']', file);
+        data() {
+            return {
+                fileData: {
+                    file_type: '',
+                    files: [],
+                },
+                allFiles: []
+            }
+        },
+        created(){
+            this.getFiles(this.orderFiles)
+        },
+        methods: {
+            getFiles(files) {
+                this.allFiles = files
+            },
+            addFiles(event) {
+                this.fileData.files = event.target.files
+            },
+            saveOrderFiles() {
+                this.$refs.fileForm.validate().then((status) => {
+                    if (status) {
+                        let formData = new FormData();
+                        for (let i = 0; i < this.fileData.files.length; i++) {
+                            let file = this.fileData.files[i];
+                            formData.append('files[' + i + ']', file);
+                        }
+                        formData.append('file_type', this.fileData.file_type)
+                        this.$boston.post('upload-order-files/' + this.orderId, formData, {
+                            headers: {
+                                'Content-Type': 'multipart/form-data'
+                            }
+                        }).then(res => {
+                            this.$root.$emit('wk_update', res.data)
+                            this.$root.$emit('wk_flow_toast', res)
+                            this.getFiles(res.orderFiles)
+                            this.$bvModal.hide('upload-files')
+                        }).catch(err => {
+                            console.log(err)
+                        })
+                    }
+                })
+            }
         }
-        formData.append('file_type',this.fileData.file_type)
-        axios.post('upload-order-files/'+ this.orderId,formData,{ headers: {
-          'Content-Type': 'multipart/form-data'
-        }})
-            .then(res => {
-              this.message = res.data.message
-              setTimeout(function(){
-                that.$bvModal.hide('upload-files')
-                that.message = ''
-              }, 2000);
-            }).catch(err => {
-              console.log(err)
-        })
-      }
     }
-  }
 </script>
