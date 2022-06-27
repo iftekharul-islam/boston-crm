2c62a28ee947a63fd40be033a05366cda0053d86
MD Minhaj <minhaj372@gmail.com>
2022-06-27 17:12:51 +0600

    Calls modal and dropdown design

--- resources/views/call/index.blade.php
+++ resources/views/call/index.blade.php
@@ -37,13 +37,35 @@
                     <span class="call-list-item ">Insp. date & time</span>
                     <span class="call-list-item ">Status</span>
                     <span class="call-list-item">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path opacity="0.25" d="M17.028 0H6.972C2.604 0 0 2.604 0 6.972V17.028C0 21.396 2.604 24 6.972 24H17.028C21.396 24 24 21.396 24 17.028V6.972C24 2.604 21.396 0 17.028 0Z" fill="#2F415E"/>
-                            <path d="M19.5727 8.244C19.5727 8.736 19.1767 9.144 18.6727 9.144H12.3727C11.8807 9.144 11.4727 8.736 11.4727 8.244C11.4727 7.752 11.8807 7.344 12.3727 7.344H18.6727C19.1767 7.344 19.5727 7.752 19.5727 8.244Z" fill="#2F415E"/>
-                            <path d="M9.56388 7.08L6.86387 9.78C6.68387 9.96 6.45588 10.044 6.22788 10.044C5.99988 10.044 5.75988 9.96 5.59188 9.78L4.69188 8.88C4.33187 8.532 4.33187 7.956 4.69188 7.608C5.03988 7.26 5.60388 7.26 5.96388 7.608L6.22788 7.872L8.29188 5.808C8.63987 5.46 9.20388 5.46 9.56388 5.808C9.91188 6.156 9.91188 6.732 9.56388 7.08Z" fill="#2F415E"/>
-                            <path d="M19.5727 16.644C19.5727 17.136 19.1767 17.544 18.6727 17.544H12.3727C11.8807 17.544 11.4727 17.136 11.4727 16.644C11.4727 16.152 11.8807 15.744 12.3727 15.744H18.6727C19.1767 15.744 19.5727 16.152 19.5727 16.644Z" fill="#2F415E"/>
-                            <path d="M9.56388 15.48L6.86387 18.18C6.68387 18.36 6.45588 18.444 6.22788 18.444C5.99988 18.444 5.75988 18.36 5.59188 18.18L4.69188 17.28C4.33187 16.932 4.33187 16.356 4.69188 16.008C5.03988 15.66 5.60388 15.66 5.96388 16.008L6.22788 16.272L8.29188 14.208C8.63987 13.86 9.20388 13.86 9.56388 14.208C9.91188 14.556 9.91188 15.132 9.56388 15.48Z" fill="#2F415E"/>
-                        </svg>
+                        <div class="dropdown">
+                            <svg id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" class="cursor-pointer" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.25" d="M17.028 0H6.972C2.604 0 0 2.604 0 6.972V17.028C0 21.396 2.604 24 6.972 24H17.028C21.396 24 24 21.396 24 17.028V6.972C24 2.604 21.396 0 17.028 0Z" fill="#2F415E"/>
+                                <path d="M19.5727 8.244C19.5727 8.736 19.1767 9.144 18.6727 9.144H12.3727C11.8807 9.144 11.4727 8.736 11.4727 8.244C11.4727 7.752 11.8807 7.344 12.3727 7.344H18.6727C19.1767 7.344 19.5727 7.752 19.5727 8.244Z" fill="#2F415E"/>
+                                <path d="M9.56388 7.08L6.86387 9.78C6.68387 9.96 6.45588 10.044 6.22788 10.044C5.99988 10.044 5.75988 9.96 5.59188 9.78L4.69188 8.88C4.33187 8.532 4.33187 7.956 4.69188 7.608C5.03988 7.26 5.60388 7.26 5.96388 7.608L6.22788 7.872L8.29188 5.808C8.63987 5.46 9.20388 5.46 9.56388 5.808C9.91188 6.156 9.91188 6.732 9.56388 7.08Z" fill="#2F415E"/>
+                                <path d="M19.5727 16.644C19.5727 17.136 19.1767 17.544 18.6727 17.544H12.3727C11.8807 17.544 11.4727 17.136 11.4727 16.644C11.4727 16.152 11.8807 15.744 12.3727 15.744H18.6727C19.1767 15.744 19.5727 16.152 19.5727 16.644Z" fill="#2F415E"/>
+                                <path d="M9.56388 15.48L6.86387 18.18C6.68387 18.36 6.45588 18.444 6.22788 18.444C5.99988 18.444 5.75988 18.36 5.59188 18.18L4.69188 17.28C4.33187 16.932 4.33187 16.356 4.69188 16.008C5.03988 15.66 5.60388 15.66 5.96388 16.008L6.22788 16.272L8.29188 14.208C8.63987 13.86 9.20388 13.86 9.56388 14.208C9.91188 14.556 9.91188 15.132 9.56388 15.48Z" fill="#2F415E"/>
+                            </svg>
+                            {{-- dropdown list --}}
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <div class="checkbox-group">
+                                    <input type="checkbox" class="checkbox-input">
+                                    <label for="" class="checkbox-label">Serial no</label>
+                                </div>
+                                <div class="checkbox-group">
+                                    <input type="checkbox" class="checkbox-input">
+                                    <label for="" class="checkbox-label">Due date</label>
+                                </div>
+                                <div class="checkbox-group">
+                                    <input type="checkbox" class="checkbox-input">
+                                    <label for="" class="checkbox-label">Serial no</label>
+                                </div>
+                                <div class="checkbox-group">
+                                    <input type="checkbox" class="checkbox-input">
+                                    <label for="" class="checkbox-label">Serial no</label>
+                                </div>
+
+                            </div>
+                        </div>
                     </span>
                 </div>
                 {{-- call item --}}
@@ -69,9 +91,9 @@
                         <span class="call-list-item">
                             <a href="#" class="icon-list" data-bs-placement="bottom" title="Details"><span class="icon-eye text-blue-eye fs-20"><span class="path1"></span><span class="path2"></span></span></a>
                             <a href="#" ata-bs-toggle="tooltip" data-bs-placement="bottom" title="Quick view" class="icon-list quick-view-icon" data-bs-toggle="modal" data-bs-target="#quickViewModal"><span class="icon-note text-purple fs-20"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></span></a>
-                            <a href="#" class="icon-list" data-bs-placement="bottom" title="Call log"><span class="icon-messages2 primary-text fs-20"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></span></a>
+                            <a href="#" class="icon-list" data-bs-placement="bottom" title="Call log"><span class="icon-messages2 primary-text fs-20" data-bs-toggle="modal" data-bs-target="#callLogModal"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></span></a>
                             <a href="#" class="icon-list" data-bs-placement="bottom" title="Schedule"><span class="icon-calendar text-brown fs-20" data-bs-toggle="modal" data-bs-target="#scheduleModal"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span></span></a>
-                            <a href="#" class="icon-list"  data-bs-placement="bottom" title="Email & SMS"> <span class="icon-messages text-yellow-msg  fs-20"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
+                            <a href="#" class="icon-list"  data-bs-placement="bottom" title="Email & SMS"> <span class="icon-messages text-yellow-msg  fs-20" data-bs-toggle="modal" data-bs-target="#sendMesaageModal"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span></a>
                             <a href="#" class="icon-list"><span class="icon-call text-light-red fs-20"><span class="path1"></span><span class="path2"></span></span></a>
                             <button class="button button-transparent p-0" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"><span class="icon-arrow-bottom"></span></button>
                         </span>
@@ -125,6 +147,7 @@
                            <p class="text-center text-white mb-0">More</p>
                         </a>
                     </div>
+                    {{-- all modal --}}
                     {{-- quick view --}}
                     <div class="modal fade quick-view-modal" id="quickViewModal" tabindex="-1" aria-labelledby="quickViewModalLabel" aria-hidden="true">
                         <div class="modal-dialog h-100">
@@ -265,6 +288,121 @@
                             <div class="modal-footer">
                               <button type="button" class="button button-transparent " data-bs-dismiss="modal">Close</button>
                               <button type="button" class="button button-primary px-5">Save</button>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                     {{-- Call log --}}
+                     <div class="modal fade schedule-modal call-log-modal" id="callLogModal" tabindex="-1" aria-labelledby="callLogModalLabel" aria-hidden="true">
+                        <div class="modal-dialog h-100">
+                          <div class="modal-content ">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <div class="modal-body h-100 overflow-auto">
+                                <p class="mgb-22 fs-20 text-600">Call summary</p>
+                                {{-- item --}}
+                              <div class="call-summary-item">
+                                <div class="top d-flex align-items-center">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpUDTIJ3qyxor41TSNxjJf3jlqk1f6E7gOmA&usqp=CAU" alt=" profile photo boston" class="img-fluid">
+                                    <div class="ms-3">
+                                        <p class="fw-bold mb-1">Mahfuz</p>
+                                        <p class="text-gray fs-12 mb-0">Today 12:15 PM</p>
+                                    </div>
+                                </div>
+                                <p class="message">Kmon achen vaiya?</p>
+                              </div>
+                              <div class="call-summary-item">
+                                <div class="top d-flex align-items-center">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpUDTIJ3qyxor41TSNxjJf3jlqk1f6E7gOmA&usqp=CAU" alt=" profile photo boston" class="img-fluid">
+                                    <div class="ms-3">
+                                        <p class="fw-bold mb-1">Mahfuz</p>
+                                        <p class="text-gray fs-12 mb-0">Today 12:15 PM</p>
+                                    </div>
+                                </div>
+                                <p class="message">Alhamdulillah vaiya,
+                                    kajer obostha khub e valo.
+                                    Just r matro koyekta din.</p>
+                              </div>
+                              <div class="call-summary-item">
+                                <div class="top d-flex align-items-center">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpUDTIJ3qyxor41TSNxjJf3jlqk1f6E7gOmA&usqp=CAU" alt=" profile photo boston" class="img-fluid">
+                                    <div class="ms-3">
+                                        <p class="fw-bold mb-1">Mahfuz</p>
+                                        <p class="text-gray fs-12 mb-0">Today 12:15 PM</p>
+                                    </div>
+                                </div>
+                                <p class="message">MashaAllah
+                                    We are very hapy to hear about 
+                                    your update. 
+                                    Thanks</p>
+                              </div>
+                              <div class="call-summary-item">
+                                <div class="top d-flex align-items-center">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpUDTIJ3qyxor41TSNxjJf3jlqk1f6E7gOmA&usqp=CAU" alt=" profile photo boston" class="img-fluid">
+                                    <div class="ms-3">
+                                        <p class="fw-bold mb-1">Mahfuz</p>
+                                        <p class="text-gray fs-12 mb-0">Today 12:15 PM</p>
+                                    </div>
+                                </div>
+                                <p class="message">Kmon achen vaiya?</p>
+                              </div>
+
+                              {{-- update log --}}
+                              <div class="update-log">
+                                <p class="mb-3 fw-bold">Update log</p>
+                                <textarea name="" id="" cols="30" rows="3" class="dashboard-textarea w-100 br-8" placeholder="Type here..."></textarea>
+                                <div class="checkbox-group mt-2">
+                                    <input type="checkbox" class="checkbox-input">
+                                    <label for="" class="checkbox-label">Call completed</label>
+                                </div>
+                              </div>
+
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="button button-primary px-5">Post</button>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                    {{-- Send messages --}}
+                    <div class="modal fade schedule-modal call-log-modal" id="sendMesaageModal" tabindex="-1" aria-labelledby="sendMesaageModalLabel" aria-hidden="true">
+                        <div class="modal-dialog h-100">
+                          <div class="modal-content ">
+                            <div class="modal-body h-100 overflow-auto">
+                                <p class="mgb-22 fs-20 text-600">Send messages</p>
+                               <div class="group">
+                                    <label for="" class="d-block mb-1">Receipant email</label>
+                                    <input type="text" class="dashboard-input w-100">
+                               </div>
+                               <div class="group">
+                                <label for="" class="d-block mb-1">Mobile no</label>
+                                <input type="text" class="dashboard-input w-100">
+                               </div>
+                               <div class="group">
+                                <label for="" class="d-block mb-1">Subject</label>
+                                <input type="text" class="dashboard-input w-100">
+                               </div>
+                               <div class="group">
+                                <label for="" class="d-block mb-1">Messages</label>
+                                <textarea name="" id="" cols="30" rows="3" class="dashboard-textarea w-100 br-8" placeholder="Type here..."></textarea>
+                               </div>
+                               <div>
+                                <p class="mgb-12">Send message to</p>
+                                  <div class="d-flex">
+                                    <div class="checkbox-group mgr-32">
+                                        <input type="checkbox" class="checkbox-input">
+                                        <label for="" class="checkbox-label">Email</label>
+                                    </div>
+                                    <div class="checkbox-group">
+                                        <input type="checkbox" class="checkbox-input">
+                                        <label for="" class="checkbox-label">SMS</label>
+                                    </div>
+                                   </div>
+                               </div>
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="button button-transparent " data-bs-dismiss="modal">Close</button>
+                              <button type="button" class="button button-primary px-5">Send</button>
                             </div>
                           </div>
                         </div>
