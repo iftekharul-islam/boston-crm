1d1b7762bc31e48eca315e557b29ed12194a4e97
iftekhar <iftrkharul@augnitive.com>
2022-07-07 21:09:16 +0600

    #205 order status update - story : https://trello.com/c/dlFaG47Q/205-client-another-feedback

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
     <div class="order bg-platinum dashboard-space">
-        <order-list :filter-type="{{ json_encode($filterType) }}" :summary="{{ json_encode($orderSummary) }}" :data="{{ json_encode($orderData) }}"></order-list>
+        <order-list :filter-type="{{ json_encode($filterType) }}" :summaryData="{{ json_encode($orderSummary) }}" :data="{{ json_encode($orderData) }}"></order-list>
     </div>
 @endsection
