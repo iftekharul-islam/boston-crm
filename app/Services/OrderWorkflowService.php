160708851e1f08b6c3b1f8d4a3f167e4ec3bf63f
mashuk-dev <mushfiqur.rahman06@gmail.com>
2022-07-27 13:55:03 +0600

    order schedule notes issue

--- app/Services/OrderWorkflowService.php
+++ app/Services/OrderWorkflowService.php
@@ -22,17 +22,16 @@ class OrderWorkflowService
         $inspector = User::where('id', $schedule->inspector_id)->select('name', 'email', 'color_id')->first();
         if ($schedule->event_id) {
             $event = Event::find($schedule->event_id);
-            $eventNote = $schedule->reschedule_note;
         } else {
             $event = new Event;
-            $eventNote = $schedule->note;
         }
-        $event->name = $property_info->search_address . ' ' . $inspector->name  . ' (' . $contact_info->contact . ',' . $phone_numbers . ') ' . $eventNote;
-        $event->description = $eventNote;
+        $event->name = $property_info->search_address . ' ' . $inspector->name  . ' (' . $contact_info->contact . ',' . $phone_numbers . ') ' . $schedule->note;
+        $event->description = $schedule->note;
         $event->startDateTime = Carbon::parse($schedule->inspection_date_time);
         $event->endDateTime = Carbon::parse($schedule->inspection_date_time)->addMinute((int) $schedule->duration);
         $event->location = $property_info->search_address;
         $event->colorId = $inspector->color_id ?? 1;
+        // Error trace : Service accounts cannot invite attendees without Domain-Wide Delegation of Authority
         // if($inspector->email){
         //     $event->addAttendee(['email' => $inspector->email]);
         // }
